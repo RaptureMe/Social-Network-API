7bb23ed66c1b834e86d38c1877bf4f837a64714b
RaptureMe <arosu4u@gmail.com>
2023-11-03 20:27:26 -0700

    completed thought and user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { Thought } = require('../models');
 const User = require('../models/User');
 
 module.exports = {
@@ -32,4 +33,44 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const thought = await Thought.findOneAndRemove({ username: req.body.username });
+
+      const user = await User.findOneAndRemove(
+        { _id: req.params.userId },
+        { new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'user created but no user with this id!' });
+      }
+
+      res.json({ message: 'user successfully deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 };
